7f96dc7019030ce068a96564a8ffe7b4a10c73a8
timsuchanek <tim.suchanek@gmail.com>
2019-08-26 17:42:41 +0200

    add photon

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,14 @@
-exports.handler = (event, ctx, cb) => {
+const Photon = require('@generated/photon').default
+
+exports.handler = async (event, ctx, cb) => {
+  const photon = new Photon()
+  const result = await photon.users.create({
+    data: {
+      username: 'Some user name',
+    },
+  })
   cb(null, {
     statusCode: 200,
-    body: 'Hello Wourld'
+    body: JSON.stringify(result),
   })
 }
-
